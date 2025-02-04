fbd149190d5737eadd126d93711c179c0e00f839
BlazeMv <ad.adamdavid72@gmail.com>
2025-02-04 20:16:52 +0500

    argument parser for env variables

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from puzzles.letter_boxed import letter_boxed_bp
 from puzzles.spelling_bee import spelling_bee_bp
+import argparse
 
 app = Flask(__name__)
 
@@ -13,4 +14,13 @@ def home():
     return render_template('home.html')  # Render the home-specific template
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(description='Run the Flask app.')
+    parser.add_argument('--port', type=int, default=5000, help='Port to run the Flask app on')
+    parser.add_argument('--debug', action='store_true', help='Run Flask in debug mode')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Run the app with the specified port and debug mode
+    app.run(port=args.port, debug=args.debug)
